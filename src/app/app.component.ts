9e042e84d7e2d959e19772dbed6c3df0102ac575
Alex Obogbare <Onozorgheneho1@>
2025-12-02 01:21:42 +0400

    update(chat): loading progess bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component} from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { NgProgressbar } from 'ngx-progressbar';
+import { NgProgressRouter } from 'ngx-progressbar/router';
 @Component({
   selector: 'app-root',
   imports: [
-    RouterModule
+    RouterModule,
+    NgProgressbar, NgProgressRouter
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
+import { progressInterceptor } from 'ngx-progressbar/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
